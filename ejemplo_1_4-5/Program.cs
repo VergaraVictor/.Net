304e60da2db81e108725ee228b16d7c2ee23555f
vvergara-hub <victorvergara1531@gmail.com>
2023-03-11 17:35:13 -0500

    UpdateEjemplo

---
+++ ejemplo_1_4-5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Introduce tu nombre:");
+
+        string nombre = Console.ReadLine();
+
+        Console.WriteLine("Hola " + nombre);
+
+        Console.ReadKey();
+
+        Console.WriteLine("Introduce tu nombre2:");
+
+        string nombre2 = Console.ReadLine();
+
+        Console.WriteLine("Introduce tu ciudad:");
+
+        string ciudad = Console.ReadLine();
+
+        Console.WriteLine("Hola " + nombre + " bienvenido a " + ciudad);
+
+        Console.ReadKey();
+
+
+    }
+}
