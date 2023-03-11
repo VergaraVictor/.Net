a9d5b105da92385b7259fb44ef5945fe25799993
vvergara-hub <victorvergara1531@gmail.com>
2023-03-11 14:49:29 -0500

    Gramatica_.net

---
+++ miconsolaNetCore/miconsolaNetCore/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace miconsolaNetCore
+    
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Codigo de Entrada
+            // Esto es el mensaje de bienvenida
+
+            var mensaje = "Hola";
+
+            /*
+             * Esto es ub comentario de varias lineas
+             * Esto es otro comentario
+             */
+
+            Console.WriteLine(mensaje);
+            #endregion 
+
+        }
+    }
+}
