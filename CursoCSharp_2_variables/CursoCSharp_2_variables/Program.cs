c2fc44e5e0b0ead7e957616ffa8b6e20504e03c7
vvergara-hub <victorvergara1531@gmail.com>
2023-03-11 16:59:30 -0500

    UPdatevariable

---
+++ CursoCSharp_2_variables/CursoCSharp_2_variables/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CursoCSharp_2_variables
+ {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String mensajes = Console.ReadLine();
+
+            string hola = null;
+
+            int edad = default(int); //0
+            int? altura = null; // al colocar ? despues del tipo de dato puede ser null que es declarla null
+            bool verdadero = default(bool); //false
+
+            edad = edad + 5;
+
+            if(edad == null)
+            {
+                //hacer algo
+            }
+
+            DateTime? fecha = new DateTime();
+            fecha = null;
+            int? dia = fecha?.Day;
+
+            Console.WriteLine(edad);
+            Console.ReadLine();
+
+        }
+    }
+}
