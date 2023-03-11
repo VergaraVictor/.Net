532da8e4e3bf27a352df732653f5ea7bfef532fc
vvergara-hub <victorvergara1531@gmail.com>
2023-03-11 17:26:00 -0500

    UpdateNomenclatura

--- CursoCSharp_3_nomenclatura/Program.cs
+++ CursoCSharp_3_nomenclatura/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        private readonly string _estaEsMiCadena; //para variables privadas se coloca al inicio _nombre
+        static void Main(string[] args)
+        {
+
+            //Camel case 
+            // Se usa cuando en varibles locales y miebros privados
+            var estoEsUnaVariable = "Camel";
+
+            //Pascal case
+            //Se usa cuando definimos tipos, metodos y clases
+            var EstoEsUnaVariable = "Pascal";
+
+        }
+
+        //Metodo
+        //Bien Buen ejemplo 
+        public int SumaDosNumeros(int numeroUno, int numeroDos)
+        {
+            var resultado = numeroUno + numeroDos;
+
+            return resultado;
+        }
+
+        //Mal ejemplo
+
+        public int Sumar(int a, int b)
+        {
+            var c = a + b;
+
+            return c;
+        }
+
+
+    }
+}
