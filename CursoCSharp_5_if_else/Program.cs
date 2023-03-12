8662f37a71147f904e5d3c7db463133ed8b7a0f1
vvergara-hub <victorvergara1531@gmail.com>
2023-03-12 07:27:04 -0500

    updateIfElse

---
+++ CursoCSharp_5_if_else/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace CursoCSharp_5_if_else
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //IF Else
+
+            bool bandera = true;
+
+            if(bandera)
+            {
+                //si bandera = true
+            }
+            else
+            {
+                //si bandera = false
+            }
+
+            int suma = 12 + 8;
+
+            if(suma <= 30)
+            {
+                //suma es menor o igual a 30
+            }
+            else
+            {
+                //suma es mayor que 30
+            }
+
+            if(suma.GetType() == typeof(int))
+            {
+               //Si suma es un entero
+            }
+
+            // Seguiria la ejecución
+
+
+            var multiplicacion = 20;
+            if (suma == 20 && bandera)
+            {
+
+                if(suma > 30)
+                {
+
+                }
+                else
+                {
+                    if(bandera)
+                    {
+
+                    }
+                }
+
+            }
+
+        }
+    }
+}
