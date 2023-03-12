d62432d8400e668411c4b7d3b48e9ab0cb110823
vvergara-hub <victorvergara1531@gmail.com>
2023-03-12 07:58:00 -0500

    updateDo

---
+++ CursoCSharp_7_do/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+/* Intruccion de interaciónes una de ella do do ejecuta un instruccion o bloques de instrucciones,
+* se evalua despues de cada ejecución del bucle con lo que se ejecuta una o varias veces.
+*/
+namespace CursoCSharp_7_do
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*do
+            *{
+            *    // Instrucciones
+            *} while (true); //Mientras se cumpla unas condiciones
+            */
+
+            int numero = 0;
+
+            do
+            {
+                Console.Write(numero);
+                numero++; //esto es igual a colocar a numero = numero + 1;
+
+                if (numero ==5)
+                {
+                    return;
+                }
+            }
+            while (numero < 10); 
+
+        }
+    }
+}
