c05fdc52cfb3a03c44533d859bc028c2035ef113
vvergara-hub <victorvergara1531@gmail.com>
2023-03-12 08:06:53 -0500

    updateWhile

---
+++ CursoCSharp_8_while/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+//Es una instrucion de interaccion, un bucle white se evalua antes de la instrucción; puede ser que no ingrese nunca
+
+namespace CursoCSharp_8_while
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0;
+
+            while (numero < 10)
+            {
+                Console.WriteLine(numero);
+                numero++;
+            }
+        }
+    }
+}
