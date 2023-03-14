51b188d8eeb2e7acacab702d3192a06e35dc9e1c
vvergara-hub <victorvergara1531@gmail.com>
2023-03-13 20:45:19 -0500

    updateArgumentos

---
+++ CursoCSharp_12_leyendo_argumentos/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CursoCSharp_12_leyendo_argumentos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hay " + args.Length + " elementos");
+
+            foreach (var argumentos in args) 
+            {
+                Console.WriteLine(argumentos);
+            }
+        }
+    }
+}
