9a58950b37a62c92e6974d3190e1eb156960cc36
vvergara-hub <victorvergara1531@gmail.com>
2023-03-13 20:11:03 -0500

    UpdateForeach

---
+++ CursoCSharp_10_foreach/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace CursoCSharp_10_foreach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var listaDeNumero = new List<int> { 0,1,2,3,4,5,6,7,8,9};
+
+            foreach (var numero in listaDeNumero)
+            {
+                Console.WriteLine(numero);
+            }
+
+            var listaDePersonas = new List<Persona>();
+
+            var persona1 = new Persona()
+            {
+                Apellido = "Sanchez",
+                Edad = 22,
+                Nombre = "Pedro"
+            };
+
+            var persona2 = new Persona()
+            {
+                Apellido = "Gomez",
+                Edad = 28,
+                Nombre = "Alejandro"
+            };
+
+            listaDePersonas.Add(persona1);
+            listaDePersonas.Add(persona2);
+
+            foreach (var persona in listaDePersonas)
+            {
+                Console.WriteLine(persona.Nombre + " " + persona.Apellido + " " + "  tiene " + persona.Edad + " años ");
+            }
+        }
+    }
+
+    public class Persona 
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set;}
+        public int Edad { get; set;}
+    }
+}
