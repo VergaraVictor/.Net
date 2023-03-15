d123ca558a3f687d792e212d4176c2b7f788e44c
vvergara-hub <victorvergara1531@gmail.com>
2023-03-14 23:03:24 -0500

    updateFunciones3

--- CursoCSharp_2_funciones/Program.cs
+++ CursoCSharp_2_funciones/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace CursoCSharp_2_funciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        //Procedimientos
+        void Procedimiento()
+        {
+            Console.Write("Esto es un procedimiento");
+        }
+        
+        //Funciones
+        int FuncionSuma()
+        {
+            return 7;
+        }
+
+        //Procedimientos de prodiedades
+
+        //Procedimientos de operador
+    }
+}
---
+++ CursoCSharp_3_funciones/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CursoCSharp_3_funciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string saludo = Saludo("Pedro","Lopez");
+        }
+        
+        public static string Saludo(string nombre, string apellidos)
+        {
+            return "Mi nombre es: " + nombre + " " + apellidos;
+        }
+    }
+}
