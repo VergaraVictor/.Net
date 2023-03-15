c52a7f3849b98f0eee2d788c32b27f43721f857f
vvergara-hub <victorvergara1531@gmail.com>
2023-03-15 13:09:59 -0500

    updateValorReferencia

--- CursoCSharp_3_funciones/Program.cs
+++ CursoCSharp_3_funciones/Program.cs
@@ -10,10 +10,28 @@ namespace CursoCSharp_3_funciones
 
             string saludo = Saludo("Pedro","Lopez");
         }
-        
+
+        //Se le pasan parametros y devuelve un valor
         public static string Saludo(string nombre, string apellidos)
         {
             return "Mi nombre es: " + nombre + " " + apellidos;
+        }
+        //Que solo ejecutan codigo
+        public static void Metodo()
+        {
+            Console.WriteLine("valor");
+        }
+
+        //Devuelve valor pero no recibe valores
+        public static int FuncionSinParametros()
+        {
+            return 7 + 9;
+        }
+
+        //Metodo, que no devuelve valor pero reciben parametros.
+        public static void MetodoConParametros(string nombre,string apellido)
+        {
+            Console.WriteLine(nombre + " " + apellido);
         }
     }
 }
